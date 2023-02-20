8ed4a052dab925d6986a47373e942cf30f2ee2a3
Wihlmott <wit.valentine@gmail.com>
2023-02-20 08:43:55 +0200

    basicLayout1

--- src/Components/Layout/Sidebar.jsx
+++ src/Components/Layout/Sidebar.jsx
@@ -1,7 +1,13 @@
 import classes from './Sidebar.module.css'
+import SidebarBtn from './Sidebar/SidebarBtn';
 
 const Sidebar = () => {
-    return <div className={classes.sidebar}>Sidebar</div>
+    const page = 'page';
+
+    return <div className={classes.sidebar}>
+        {page !== 'profile' && <div className={classes.profilePic}></div>}
+        <SidebarBtn/>
+    </div>
 }
 
 export default Sidebar;
---
+++ src/Components/Layout/Sidebar/SidebarBtn.jsx
@@ -0,0 +1,7 @@
+import classes from './SidebarBtn.module.css'
+
+const SidebarBtn = ({title}) => {
+    return <div className={classes.sidebarBtn}>text</div>
+}
+
+export default SidebarBtn;
