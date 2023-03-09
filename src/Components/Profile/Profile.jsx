3fbc1bbe605957cc5e13cd3cfb202274fb209793
Wihlmott <wit.valentine@gmail.com>
2023-03-09 22:22:03 +0200

    profilePage

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,14 +1,20 @@
 import InfoStrip from "./InfoStrip";
+import Button from "@mui/material/Button";
 
 const profile_dummy = {firstName: 'wihl',lastName: 'valentine',email: 'test@email.com',cell: '021 987 9879'}
 
 const Profile = () => {
     const entries = Object.entries(profile_dummy);
 
+    const saveNewDataHandler = () => {
+        console.log(`update data`);
+    }
+
     return <div>
         {entries.map((el)=>{
             return <InfoStrip infoTitle={el[0]} information={el[1]}/>
         })}
+        <Button sx={{m:2}} variant='contained' onClick={saveNewDataHandler}>save</Button>
     </div>
 }
 
