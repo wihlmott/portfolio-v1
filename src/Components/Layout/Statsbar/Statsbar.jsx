657c7e725c47fd9158bfdf502824620a9ee96b2d
Wihlmott <wit.valentine@gmail.com>
2023-03-09 14:37:53 +0200

    progressStatsFirst

--- src/Components/Layout/Statsbar/Statsbar.jsx
+++ src/Components/Layout/Statsbar/Statsbar.jsx
@@ -1,14 +1,35 @@
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import classes from './Statsbar.module.css'
+import LinearProgress from '@mui/material/LinearProgress';
+
+const completedData_dummy = [{educator: 'starr hendricks', data: '', date:'23Feb2023'}]
+let totalAmount = 2;
+const progress = (completedData_dummy.length/totalAmount)*100;
 
 const Statsbar = () => {
     return <div className={classes.statsbar}>
         <Paper className={classes.progress}>
-            <Typography variant='h8' color={'grey'} borderBottom={'1px solid grey'}>progress</Typography>
+            <Typography color={'rgba(128, 128, 128, 0.7)'} borderBottom={'0.1px solid rgba(128, 128, 128, 0.2)'}>progress</Typography>
+            <br/>
+            <div className={classes.progressBarONE}>
+                <Typography variant='subtitle' color={'grey'} m={1}>first forms progress</Typography>
+                <LinearProgress variant='determinate' value={progress}/>
+            </div>
+            <br/>
+            <div className={classes.progressBarTWO}>
+                <Typography variant='subtitle' color={'grey'} m={1}>second forms progress</Typography>
+                <LinearProgress variant='determinate' value={progress}/>
+            </div>
         </Paper>
         <Paper className={classes.history}>
-            <Typography variant='h8' color={'grey'} borderBottom={'1px solid grey'}>history</Typography>
+            <Typography color={'rgba(128, 128, 128, 0.7)'} borderBottom={'1px solid rgba(128, 128, 128, 0.2)'}>history</Typography>
+            {completedData_dummy.map((el)=>{
+                return <div className={classes.historyTxt} key={`${el.educator}--${el.date}`}>
+                    <Typography variant='subtitle' color={'grey'}>{`${el.educator} -- ${el.date}` }</Typography>
+                    <br/>
+                    </div>
+            })}
         </Paper>
     </div>
 }
