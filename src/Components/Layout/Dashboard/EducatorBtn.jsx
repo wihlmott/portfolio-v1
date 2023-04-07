ebbecc4728caa28ca5bb858fb117912a97088098
Wihlmott <wit.valentine@gmail.com>
2023-04-07 13:51:47 +0200

    styling_changes-Dash

--- src/Components/Layout/Dashboard/EducatorBtn.jsx
+++ src/Components/Layout/Dashboard/EducatorBtn.jsx
@@ -18,42 +18,53 @@ const EducatorBtn = (props) => {
   };
 
   return (
-    <Card elevation={2} onClick={clickHandler}>
-      <CardHeader
-        action={
-          <>
-            <IconButton>
-              <FolderOpenIcon
-                data-type={"PLANNING_CHECK_FORM"}
-                sx={{
-                  m: "-10px",
-                  fontSize: "3rem",
-                  p: "10px",
-                  backgroundColor: "linear-gradient(145deg, #f0f0f0, #cacaca)",
-                  boxShadow: "5px 5px 12px #bebebe,-5px -5px 12px #ffffff",
-                  borderRadius: 50,
-                }}
-              />
-            </IconButton>
-            <IconButton>
-              <ContentPasteIcon
-                data-type={"ASSESSMENT_CHECK_FORM"}
-                sx={{
-                  m: "-10px",
-                  ml: "5px",
-                  fontSize: "3rem",
-                  p: "10px",
-                  backgroundColor: "linear-gradient(145deg, #f0f0f0, #cacaca)",
-                  boxShadow: "5px 5px 12px #bebebe,-5px -5px 12px #ffffff",
-                  borderRadius: 50,
-                }}
-              />
-            </IconButton>
-          </>
-        }
-        subheader={props.educator}
-      />
-    </Card>
+    <div
+      style={{
+        boxShadow: "0px 0px 10px 2px rgba(63,80,181,0.35)",
+        margin: "0 5px",
+      }}
+    >
+      <Card elevation={2} onClick={clickHandler}>
+        <CardHeader
+          action={
+            <>
+              <IconButton>
+                <FolderOpenIcon
+                  data-type={"PLANNING_CHECK_FORM"}
+                  sx={{
+                    m: "-10px",
+                    fontSize: "3rem",
+                    p: "10px",
+                    backgroundColor:
+                      "linear-gradient(145deg, #f0f0f0, #cacaca)",
+                    boxShadow: "1px 1px 10px -2px rgba(0,0,0,0.6) inset",
+                    // boxShadow: "5px 5px 12px #bebebe,-5px -5px 12px #ffffff",
+                    borderRadius: 50,
+                  }}
+                />
+              </IconButton>
+              <IconButton>
+                <ContentPasteIcon
+                  data-type={"ASSESSMENT_CHECK_FORM"}
+                  sx={{
+                    m: "-10px",
+                    ml: "5px",
+                    fontSize: "3rem",
+                    p: "10px",
+                    backgroundColor:
+                      "linear-gradient(145deg, #f0f0f0, #cacaca)",
+                    // boxShadow: "5px 5px 12px #bebebe,-5px -5px 12px #ffffff",
+                    boxShadow: "1px 1px 10px -2px rgba(0,0,0,0.6) inset",
+                    borderRadius: 50,
+                  }}
+                />
+              </IconButton>
+            </>
+          }
+          subheader={props.educator}
+        />
+      </Card>
+    </div>
   );
 };
 
